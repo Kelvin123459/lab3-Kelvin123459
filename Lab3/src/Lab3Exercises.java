65a2d445d0bde53d50114c9666546482f9b07540
Kelvi <Kelvi@10.31.65.98>
2017-02-08 10:32:01 -0400

    Set to center

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,7 +4,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Kelvin Garcia");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(550, 300);
+		myFrame.setLocationRelativeTo(null);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
